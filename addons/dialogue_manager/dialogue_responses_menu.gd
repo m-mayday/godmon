a84f30d79d945e3e2ab1ea579d956b8a4c9b11cf
m-mayday <iwanowsacha+git@gmail.com>
2025-05-16 17:38:20 -0300

    Extend DialogueManager addon to choose a 'cancel' response with keyboard

--- addons/dialogue_manager/dialogue_responses_menu.gd
+++ addons/dialogue_manager/dialogue_responses_menu.gd
@@ -131,13 +131,16 @@ func _on_response_mouse_entered(item: Control) -> void:
 
 func _on_response_gui_input(event: InputEvent, item: Control, response) -> void:
 	if "Disallowed" in item.name: return
-
+	
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 		get_viewport().set_input_as_handled()
 		response_selected.emit(response)
 	elif event.is_action_pressed(&"ui_accept" if next_action.is_empty() else next_action) and item in get_menu_items():
 		get_viewport().set_input_as_handled()
 		response_selected.emit(response)
-
+	elif event.is_action_pressed("cancel"):
+		for resp in responses:
+			if resp.tags.has("cancel"):
+				response_selected.emit(resp)
 
 #endregion
